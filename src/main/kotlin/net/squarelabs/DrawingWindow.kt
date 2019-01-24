13bdb9ec6a7442be3e3d279c464da3d1bfebceaa
Brent Gardner <bgardner@squarelabs.net>
2019-01-24 06:17:46 -0700

    mouse events

--- src/main/kotlin/net/squarelabs/DrawingWindow.kt
+++ src/main/kotlin/net/squarelabs/DrawingWindow.kt
@@ -3,17 +3,50 @@ package net.squarelabs
 import java.awt.Color
 import java.awt.Dimension
 import java.awt.Graphics
+import java.awt.event.MouseEvent
+import java.awt.event.MouseListener
+import java.awt.event.MouseMotionListener
 import javax.swing.JFrame
 
-class DrawingWindow : JFrame() {
+class DrawingWindow : JFrame(), MouseListener, MouseMotionListener {
 
     init {
         defaultCloseOperation = EXIT_ON_CLOSE
         size = Dimension(400, 400)
         isVisible = true
+        addMouseListener(this)
+        addMouseMotionListener(this)
     }
 
     override fun paint(p0: Graphics?) {
         super.paint(p0)
+    }
+
+    override fun mouseReleased(p0: MouseEvent?) {
+        println("mouseReleased")
+    }
+
+    override fun mouseEntered(p0: MouseEvent?) {
+        println("mouseEntered")
+    }
+
+    override fun mouseClicked(p0: MouseEvent?) {
+        println("mouseClicked")
+    }
+
+    override fun mouseExited(p0: MouseEvent?) {
+        println("mouseExited")
+    }
+
+    override fun mousePressed(p0: MouseEvent?) {
+        println("mousePressed")
+    }
+
+    override fun mouseMoved(p0: MouseEvent?) {
+        //println("mouseMoved")
+    }
+
+    override fun mouseDragged(p0: MouseEvent?) {
+        //println("mouseDragged")
     }
 }
